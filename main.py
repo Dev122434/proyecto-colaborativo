fa12bb3aa8300f837c8f531a00966f2a68c5da09
Dev122434 <68534leo@universidaddeleon.edu.mx>
2025-09-18 08:03:01 -0600

    Funcionalidad implementada

---
+++ main.py
@@ -0,0 +1,12 @@
+from clases.avanzadas import Calculadora
+
+def main():
+    calculadora = Calculadora()
+    calculadora.leerNumeros()
+    calculadora.elevarPotencia()
+    calculadora.mostrarResultado()
+    
+if __name__ == "__main__":
+    main()
+    
+    
